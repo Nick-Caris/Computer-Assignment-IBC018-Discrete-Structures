e31a9f75ded8dcdcdf044297399b2e18f5cb68ef
Nick Caris <nickcaris97@gmail.com>
2018-12-17 02:29:20 +0100

    written the functions needed to reqrite the recurence function

--- toolbox/co_rr_solver.py
+++ toolbox/co_rr_solver.py
@@ -20,31 +20,38 @@ DESCRIPTION
         print debugging information during execution of "co_rr_solver"
 """
 
-import glob # Library for filename pattern-matching
+import glob  # Library for filename pattern-matching
 import sympy as sy
 from sympy import sympify, roots, solve, expand, factor
 from sympy.abc import r, n
-import sys # For access to the given argument
+import sys  # For access to the given argument
 import os  # Gives access to current location of co_rr_solver
 
-
 # Global variables:
-next_symbolic_var_index = 0 # This variable indicates the next index for the p_x variable names needed for Theorem 6.
-print_debug_information = False # This variable indicates whether debug information should be printed (this is read in using the command line argument list)
+next_symbolic_var_index = 0  # This variable indicates the next index for the p_x variable names needed for Theorem 6.
+print_debug_information = False  # This variable indicates whether debug information should be printed (this is read in using the command line argument list)
 
 """Print the given list line by line, each line started and ended with a quotation mark."""
+
+
 def print_list(listing):
     for line in listing:
         print("\"" + line + "\"")
 
+
 """Print the dictionary element per element: First key, then ":" and value."""
+
+
 def print_dict(dictionary):
     for key in dictionary:
         print(str(key) + ": " + str(dictionary[key]))
 
+
 """First checks if debug printing is allowed.
    Then checks the type of the input of the function.
    Then prints the input based on the type of input."""
+
+
 def debug_print(debug_information):
     global print_debug_information
     if print_debug_information:
@@ -55,26 +62,32 @@ def debug_print(debug_information):
         else:
             print(str(debug_information))
 
+
 """Determines for each line in lines:
     The x-value of s(x) and the corresponding y-value of s(x)=y.
     This is returned as dictionary where x is the integer-key and y the string-value."""
+
+
 def det_init_conditions(lines):
     conditions = {}
     for line in lines:
-        pos_s_bracket = line.find("s(") # Position of "s("
-        start_index_nr = pos_s_bracket + 2 # First index of x-value
-        pos_bracket_equal = line.find(")=", pos_s_bracket) # Position of ")="
-        start_index_y = pos_bracket_equal + 2 # First position after the "=" symbol
+        pos_s_bracket = line.find("s(")  # Position of "s("
+        start_index_nr = pos_s_bracket + 2  # First index of x-value
+        pos_bracket_equal = line.find(")=", pos_s_bracket)  # Position of ")="
+        start_index_y = pos_bracket_equal + 2  # First position after the "=" symbol
         x_value = int(line[start_index_nr:pos_bracket_equal])
         y_value = line[start_index_y:]
         conditions[x_value] = y_value
     return conditions
 
+
 """Searches for the left begin of the term (beginning at start) and returns the first position belonging to the term, where the symbols are still
     counted as part of the term (may be handy for "+" and "-", but REMIND THIS if the symbols list also contains "*" and "/")..
     The begin of a new term is indicated with one of the symbols in the list "symbols", but only if there are no opened brackets at this position."""
+
+
 def search_left_term_begin(equation, start, symbols):
-    bracket_count = 0 # Indicating the number of opened bracket-scopes
+    bracket_count = 0  # Indicating the number of opened bracket-scopes
     index = start
     while index >= 0:
         if equation[index] == ")":
@@ -84,12 +97,15 @@ def search_left_term_begin(equation, start, symbols):
         elif bracket_count == 0 and equation[index] in symbols:
             return index
         index -= 1
-    return 0 # If we got until here the term starts at the begin of equation
+    return 0  # If we got until here the term starts at the begin of equation
+
 
 """Searches for the right end of the term (beginning at start) and returns the last position belonging to the term.
     The begin of a new term is indicated with one of the symbols in the list "symbols", but only if there are no opened brackets at this position."""
+
+
 def search_right_term_end(equation, start, symbols):
-    bracket_count = 0 # Indicating the number of opened bracket-scopes
+    bracket_count = 0  # Indicating the number of opened bracket-scopes
     index = start
     while index < len(equation):
         if equation[index] == "(":
@@ -99,16 +115,74 @@ def search_right_term_end(equation, start, symbols):
         elif bracket_count == 0 and equation[index] in symbols and index > 0:
             return index - 1
         index += 1
-    return len(equation) - 1 # If we got until here the term ends at the end of equation
+    return len(equation) - 1  # If we got until here the term ends at the end of equation
+
 
 """Determines and returns:
     1. The value of x in s(n-x) as integer, where pos_s should be the index of "s" in equation
     2. equation where "s(n-x)" is replaced by "1"."""
+
+
 def recurrent_step_length(equation, pos_s):
     exclusive_end_pos = equation.find(")", pos_s)
     value = equation[pos_s + 4:exclusive_end_pos]
-    equation = equation.replace("s(n-" + value + ")", "1") # Replace "s(n-x)" with "1"
+    equation = equation.replace("s(n-" + value + ")", "1")  # Replace "s(n-x)" with "1"
     return int(value), equation
+
+
+def test(equation):
+    operators = ["-", "+", "*"]
+    start = 0
+    end = equation.find(")") + 1
+    for letter in equation:
+        if letter not in operators:
+            start = start + 1
+        else:
+            break
+
+    function_bit = equation[start: end]
+    equation = equation.replace(function_bit, "")
+
+    return equation, function_bit
+
+
+def getKey(item):
+    return item[0]
+
+
+def flatten_tuple(tuple_list):
+    string = ""
+    for item in tuple_list:
+        string += item[1]
+    return string
+
+
+def rewrite_equation(equation):
+    tuple_function = []
+    for s in equation:
+        exclusive_end_pos = s.find(")", 0)
+        value = s[exclusive_end_pos - 1:exclusive_end_pos]
+        tuple_function = tuple_function + [[value, s]]
+    tuple_function = sorted(tuple_function, key=getKey)
+
+    return flatten_tuple(tuple_function)
+
+
+def function_name_not_found(equation):
+    equation = equation[5:len(equation)]  # Remove the "s(n)="-part
+    split_up_function = []
+    pos_s = equation.find("s(n-")  # First position of recurrent part
+    while pos_s >= 0:  # There is another recurrent s(n-x) part
+        equation, function_bit = test(equation)
+        split_up_function.append(function_bit)
+        print('test', function_bit)
+        print('HIER GODVERDOMME', equation)
+        pos_s = equation.find("s(n-")  # Next position of recurrent part
+
+    print('end:', split_up_function)
+    rewritten_equation = rewrite_equation(split_up_function)
+    return rewritten_equation
+
 
 
 """Determines and returns:
@@ -116,46 +190,60 @@ def recurrent_step_length(equation, pos_s):
         -The integer-key is x of s(n-x) (thus without minus)
         -The string-value is y of y*s(n-x)
     2. A list of string-terms of F(n)."""
+
+
 def analyze_recurrence_equation(equation):
     associated = {}
-    f_n_list = []
-    equation = equation[5:len(equation)] # Remove the "s(n)="-part
-    pos_s = equation.find("s(n-") # First position of recurrent part
-    while pos_s >= 0: # There is another recurrent s(n-x) part
+    f_n_list = function_name_not_found(equation)
+    equation = equation[5:len(equation)]  # Remove the "s(n)="-part
+    pos_s = equation.find("s(n-")  # First position of recurrent part
+    while pos_s >= 0:  # There is another recurrent s(n-x) part
         debug_print(equation)
-        step_length, equation = recurrent_step_length(equation, pos_s) # Determines step length and replaces recurrent part with a "1"
+        step_length, equation = recurrent_step_length(equation,
+                                                      pos_s)  # Determines step length and replaces recurrent part with a "1"
         debug_print(step_length)
         left_pos = search_left_term_begin(equation, pos_s, ["+", "-"])
         right_pos = search_right_term_end(equation, pos_s, ["+", "-"])
-        c_n = equation[left_pos:right_pos + 1] # Substring with both indexes inclusive
-        debug_print("c_n "+ c_n)
-        equation = equation.replace(c_n, "", 1) # Remove the actual c_n from the equation (only once)
-        associated[step_length] = c_n # Add the recursive step length and factor to the dictionary
-        pos_s = equation.find("s(n-") # First position of recurrent part (because other "s(n-"-part is already removed)
+        c_n = equation[left_pos:right_pos + 1]  # Substring with both indexes inclusive
+        debug_print("c_n " + c_n)
+        equation = equation.replace(c_n, "", 1)  # Remove the actual c_n from the equation (only once)
+        associated[step_length] = c_n  # Add the recursive step length and factor to the dictionary
+        pos_s = equation.find("s(n-")  # First position of recurrent part (because other "s(n-"-part is already removed)
     # Sorry, but you will have to implement the treatment of F(n) yourself!
+
     return associated, f_n_list
+
 
 """Reads in all lines of the file except the first, second and last one.
     The lines are returned as list of strings."""
+
+
 def read_file(filename):
     lines = []
     with open(filename, "r") as input_file:
         for index, line in enumerate(input_file):
-            if not (index in [0, 1]) and line != "];\n": # Filter out first and second row and the last that contains "];\n"
-                lines.append(line.strip()) # Append and remove leading and closing whitspaces
+            if not (index in [0,
+                              1]) and line != "];\n":  # Filter out first and second row and the last that contains "];\n"
+                lines.append(line.strip())  # Append and remove leading and closing whitspaces
     return lines
+
 
 """Goes through all rows except the last and delete the "," at the end.
     The result is returned (again as list of strings)."""
+
+
 def clear_commas(lines):
     for index, line in enumerate(lines):
-        if index < len(lines) - 1: # This is not the last line
-            comma_pos = len(line) - 1 # The last index position where the "," stands
+        if index < len(lines) - 1:  # This is not the last line
+            comma_pos = len(line) - 1  # The last index position where the "," stands
             lines[index] = line[:comma_pos]
     return lines
 
+
 """Deletes all remaining whitespace and converts "^" to "**".
     The result is returned (again as list of strings)."""
+
+
 def fix_syntax(lines):
     for index, line in enumerate(lines):
         line = str.replace(line, " ", "")
@@ -163,32 +251,44 @@ def fix_syntax(lines):
         lines[index] = line
     return lines
 
+
 """Finds a closed formula for a homogeneous recurrence relation.
     The return value is a string of the right side of the equation "s(n) = ..."""
+
+
 def solve_homogeneous_equation(init_conditions, associated):
     # You have to implement this yourself!
     return result
+
 
 """Finds a closed formula for a nonhomogeneous equation, where the nonhomogeneous part consists
     of a linear combination of constants, "r*n^x" with r a real number and x a positive natural number,
     and "r*s^n" with r and s being real numbers.
     The return value is a string of the right side of the equation "s(n) = ..."""
+
+
 def solve_nonhomogeneous_equation(init_conditions, associated, f_n_list):
     # You have to implement this yourself!
     return result
 
+
 """Transforms the string equation, that is of the right side of the form "s(n) = ...",
     and wirtes it towards the file "filename", which also needs to contain the desired path."""
+
+
 def write_output_to_file(filename, equation):
     nr_written_chars = 0
     with open(filename, "w") as output_file:
         nr_written_chars = output_file.write("sdir := n -> {0};\n".format(equation))
     debug_print("Wrote {0} characters to file {1}.".format(str(nr_written_chars), filename))
 
+
 """Reformats the for Python needed syntax of equations back to specified output format:
     - "**" is transformed back to "^";
     - "sqrt(...)" is transformed back to "(...)^(1/2)".
     The return value is a string of the modified equation."""
+
+
 def reformat_equation(equation):
     equation = equation.replace("**", "^")
     pos_sqrt = equation.find("sqrt(")
@@ -198,6 +298,7 @@ def reformat_equation(equation):
         equation = equation.replace("sqrt", "", 1)
         pos_sqrt = equation.find("sqrt(")
     return equation
+
 
 # Begin of program:
 if len(sys.argv) > 3:
@@ -219,9 +320,9 @@ else:
         if sys.argv[argv_index].find("/") != -1:
             path = sys.argv[argv_index]
     print(path)
-    for filename in glob.glob(path + "comass[0-9][0-9].txt"):
-        print("File: "+filename)
-        next_symbolic_var_index = 0 # Reset this index for every file
+    for filename in glob.glob("../testData/comass03.txt"):
+        print("File: " + filename)
+        next_symbolic_var_index = 0  # Reset this index for every file
         debug_print("Beginning for file \"{0}\"".format(filename))
         lines = read_file(filename)
         lines = clear_commas(lines)
@@ -232,7 +333,8 @@ else:
         tmp = len(lines)
         if lines[len(lines) - 1] == "":
             tmp -= 1
-        init_conditions = det_init_conditions([lines[index] for index in range(1, tmp)]) # Determine initial conditions with all but the first line as input
+        init_conditions = det_init_conditions([lines[index] for index in range(1,
+                                                                               tmp)])  # Determine initial conditions with all but the first line as input
         associated, f_n_list = analyze_recurrence_equation(lines[0])
 
         # Print debugging information:
@@ -243,11 +345,12 @@ else:
         debug_print(associated)
         debug_print("F(n):")
         debug_print(f_n_list)
+        break
 
         output_filename = filename.replace(".txt", "-dir.txt")
         resulting_equ = ""
         # Check if the equation is a homogeneous relation
-        if not f_n_list: # The list is empty
+        if not f_n_list:  # The list is empty
             resulting_equ = solve_homogeneous_equation(init_conditions, associated)
         else:
             resulting_equ = solve_nonhomogeneous_equation(init_conditions, associated, f_n_list)
